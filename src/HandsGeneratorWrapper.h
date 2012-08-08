8cb94c716e9d6b8423c704456b424d800439b71c
mroja <alex@mroja.net>
2012-08-08 17:15:19 +0200

    define explicit calling convention (needed for MSVC)

--- src/HandsGeneratorWrapper.h
+++ src/HandsGeneratorWrapper.h
@@ -35,8 +35,8 @@ void HandsGenerator_StartTracking_wrapped(xn::HandsGenerator& self, BP::list poi
 void HandsGenerator_SetSmoothing_wrapped(xn::HandsGenerator& self, XnFloat fSmoothingFactor);
 
 /** Internal callback implementations **/
-void Create_callback(xn::HandsGenerator& src, XnUserID user, const XnPoint3D *pPosition, XnFloat fTime, void* cookie);
-void Update_callback(xn::HandsGenerator& src, XnUserID user, const XnPoint3D *pPosition, XnFloat fTime, void* cookie);
-void Destroy_callback(xn::HandsGenerator& src, XnUserID user, XnFloat fTime, void* cookie);
+void XN_CALLBACK_TYPE Create_callback(xn::HandsGenerator& src, XnUserID user, const XnPoint3D *pPosition, XnFloat fTime, void* cookie);
+void XN_CALLBACK_TYPE Update_callback(xn::HandsGenerator& src, XnUserID user, const XnPoint3D *pPosition, XnFloat fTime, void* cookie);
+void XN_CALLBACK_TYPE Destroy_callback(xn::HandsGenerator& src, XnUserID user, XnFloat fTime, void* cookie);
 
 #endif	/* HANDS_GENERATOR_WRAPPER_H */
